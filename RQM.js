a522bb21904216c199b3801d13425a8c05af70b8
junhong0113 <junhong.cfa@gmail.com>
2020-07-07 01:10:57 +0800

    react

--- RQM.js
+++ RQM.js
@@ -1,5 +1,4 @@
 /*Redux*/
-
 const QUOTE = "quote";
 
 const changeQuote = (newquote) => ({
@@ -23,3 +22,26 @@ const quoteReducer = (state=defaultq,action) => {
 };
 
 const store = Redux.createStore(quoteReducer);
+
+/*React*/
+
+class QuoteMachine extends React.Component {
+    constructor(props){
+        super(props);
+    }
+
+    componentDidMount(){
+        fetch("https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json").then(response=>response.json()).then(data =>
+       {const currentQuote = data.quotes[Math.floor(Math.random() * data.quotes.length)];
+        this.props.generateRandomQuote(currentQuote);
+    })};
+
+    render(){
+        return (
+            <div>
+        <p>{this.props.author}</p>
+        <p>{this.props.quote}</p>
+            </div>
+        );
+    }
+}
